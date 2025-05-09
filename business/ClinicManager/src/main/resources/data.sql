61c1b5a3349182444c90c9fa98f801c6d4187c30
ArmandoSJ <just-jauregui@hotmail.com>
2025-05-09 16:24:20 -0600

    Added initial clinics data

--- business/ClinicManager/src/main/resources/data.sql
+++ business/ClinicManager/src/main/resources/data.sql
@@ -4,3 +4,10 @@ INSERT INTO doctors(doctor_id, name, last_name_paternal, last_name_maternal, spe
 (nextval('doctors_sequence'), 'Oscar', 'Valle', 'Flores', 'Endocrinología'),
 (nextval('doctors_sequence'), 'Carolina', 'Beltran', 'Lopez', 'Psiquiatría'),
 (nextval('doctors_sequence'), 'Nelson', 'Pineda', 'Peña', 'Pediatría');
+
+INSERT INTO clinics(clinic_id, room_number, floor) VALUES
+(nextval('clinics_sequence'), '14', 2),
+(nextval('clinics_sequence'), '52', 6),
+(nextval('clinics_sequence'), '25', 3),
+(nextval('clinics_sequence'), '42', 5),
+(nextval('clinics_sequence'), '30', 4);
