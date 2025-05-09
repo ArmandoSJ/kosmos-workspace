b58d1dae7ad3092b8a7e5b3447a1d25952621c50
ArmandoSJ <just-jauregui@hotmail.com>
2025-05-09 16:19:48 -0600

    Added initial doctor data

---
+++ business/ClinicManager/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO doctors(doctor_id, name, last_name_paternal, last_name_maternal, specialty) VALUES
+(nextval('doctors_sequence'), 'Armando', 'Salazar', 'Jauregui', 'Medicina Interna'),
+(nextval('doctors_sequence'), 'Miriam', 'Garcia', 'Flores', 'Cardiología'),
+(nextval('doctors_sequence'), 'Oscar', 'Valle', 'Flores', 'Endocrinología'),
+(nextval('doctors_sequence'), 'Carolina', 'Beltran', 'Lopez', 'Psiquiatría'),
+(nextval('doctors_sequence'), 'Nelson', 'Pineda', 'Peña', 'Pediatría');
