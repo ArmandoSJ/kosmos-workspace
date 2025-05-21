6841ace3c86b3282148358606abc55397c01dd11
ArmandoSJ <just-jauregui@hotmail.com>
2025-05-20 21:34:10 -0600

    add commons dependency

--- business/ClinicManager/src/main/java/com/technologyos/ClinicManager/ClinicManagerApplication.java
+++ business/ClinicManager/src/main/java/com/technologyos/ClinicManager/ClinicManagerApplication.java
@@ -3,7 +3,7 @@ package com.technologyos.ClinicManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.technologyos.*"})
 public class ClinicManagerApplication {
 
 	public static void main(String[] args) {
